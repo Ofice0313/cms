3fdf8f858f3f9ba54f940685ad61dc8cccdba3db
Marcelo Caleb Júlio Ofice <marcelocalebjulioo@gmail.com>
2026-01-21 01:50:59 +0200

    Refactorando o refreshToken

--- src/main/java/com/mcts/cms/CmsApplication.java
+++ src/main/java/com/mcts/cms/CmsApplication.java
@@ -15,7 +15,7 @@ public class CmsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CmsApplication.class, args);
 
-		generateHashedPassword();
+		//generateHashedPassword();
 	}
 
 	private static void generateHashedPassword() {
